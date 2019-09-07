6e27e47a241aead79d5df98490b8c2ec1f294f4b
shandryll <shandryll94@gmail.com>
2019-09-07 12:56:11 -0300

    Insercao da validacao do automato na classe propria

--- automaton.py
+++ automaton.py
@@ -1,10 +1,43 @@
+from symbol_table import SymbolTable
+
+
 class Automaton:
 
-    def __init__(self, state, char):
-        self.state = state
-        self.char = char
+    def __init__(self):
+        self.state = 1
+        self.lexema = ""
+        self.current_line = 1
+        self.current_column = 1
+        self.symbol_table = SymbolTable()
 
-    def verify_automaton(self, state, char=None):
+    def verify_automaton(self, char):
+
+        if not char == '\n':
+            if not char:
+                print(self.symbol_table.add_lexema(self.lexema) + " - line: " + str(self.current_line) + ", column: " + str(self.current_column) + '>')
+                return True
+
+            elif char == ' ':
+                print(self.symbol_table.add_lexema(self.lexema) + " - line: " + str(self.current_line) + ", column: " + str(self.current_column) + '>')
+                self.lexema = ''
+
+            else:
+                self.lexema = self.lexema + char
+
+            self.current_column = self.current_column + 1
+
+        elif char == '\n':
+            print(self.symbol_table.add_lexema(self.lexema) + " - line: " + str(self.current_line) + ", column: " + str(self.current_column) + '>')
+            self.lexema = ''
+            self.current_column = 1
+            self.current_line = self.current_line + 1
+
+        return False
+
+
+
+'''
+
         while True:
             if state == 1:
                 if char == '':
@@ -14,3 +47,4 @@ class Automaton:
                 elif char == ' ' or char == '\t' or char == '\n' or char == '\r':
                     self.state = 1
 
+'''
--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-from symbol_table import SymbolTable
+from automaton import Automaton
 
 
 def reader_file(filepath):
     with open(filepath) as f:
 
-        symbol_table = SymbolTable()
-
-        lexema = ""
-        current_line = 1
-        current_column = 1
-
-        while True:
+        eof = False
+        automaton = Automaton()
+        while eof is False:
             char = f.read(1)
 
-            if not char == '\n':
-                if not char:
-                    print(symbol_table.add_lexema(lexema) + " - line: " + str(current_line) + ", column: " + str(current_column) + '>')
-                    break
-                elif char == ' ':
-                    print(symbol_table.add_lexema(lexema) + " - line: " + str(current_line) + ", column: " + str(current_column) + '>')
-                    lexema = ''
-                else:
-                    lexema = lexema + char
-
-                current_column = current_column + 1
-
-            elif char == '\n':
-                print(symbol_table.add_lexema(lexema) + " - line: " + str(current_line) + ", column: " + str(current_column) + '>')
-                lexema = ''
-                current_column = 1
-                current_line = current_line + 1
+            eof = automaton.verify_automaton(char)
 
 
 if __name__ == '__main__':
